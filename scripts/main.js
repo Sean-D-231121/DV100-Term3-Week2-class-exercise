cb9e82d5ab653a74c1ffdee0f6273289bf8756b6
Sean-D-231121 <231121@virtualwindow.co.za>
2023-08-02 08:57:02 +0200

    Added Wishlist page and second slide
    Added working table for Wishlist page

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,38 @@
 $(document).ready(function(){
 
     console.log("Hello");
+    // ---------------------------------------
+    // Home
 
     // When the document loads, animate the hero image upwards
     
-    if ($("#carouselExample .carousel-inner .carousel-item").is(":visible")) {
-      $(".hero-image-2").animate({ top: "-100px" });
-      $(".hero-image").animate({ top: "-100px" });
-    }
+    
+
+    //Hide all description text from the plant cards
+    $(".description").hide();
+    //When  a card is clicked
+
+   
+
+    
 }); 
+if ($("#carouselExample .carousel-inner .carousel-item").is(":visible")) {
+  $(".hero-image-2").animate({ top: "-30px" });
+  $(".hero-image").animate({ top: "-30px" });
+}
+ $(".card").click(function () {
+   //  toggle price and description text
+   $(this).find(".card-text.price").toggle()
+   
+   $(this).find(".description").toggle();
+   // Res the image top
+   $(this).children(".card-img-top").toggleClass("small");
+ });
+//  Removes the nearest "table-row" to the div with class .removeButton
+$(".removeButton").click(function(){
+   $(this).closest('.table-row').remove();
+
+})
+
  
  
